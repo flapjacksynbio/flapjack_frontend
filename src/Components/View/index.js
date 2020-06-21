a2d3b0791a2d0181145c33b6d298884871f824b6
Tim Rudge <timrudge@gmail.com>
2020-06-20 20:40:54 -0400

    Change uuid import to v1 and subsequent function call

--- src/Components/View/index.js
+++ src/Components/View/index.js
@@ -6,7 +6,8 @@ import AddTab from './AddTab'
 import DataView from './DataView'
 import { createTab, editTab, deleteTab } from '../../redux/actions/viewTabs'
 import { connect } from 'react-redux'
-import uuid from 'uuid'
+//import uuid from 'uuid'
+import {v1 as uuid} from "uuid"; 
 import PropTypes from 'prop-types'
 
 const View = ({ tabs, createTab, editTab, deleteTab }) => {
@@ -53,7 +54,7 @@ const View = ({ tabs, createTab, editTab, deleteTab }) => {
     }
 
     const title = `${name} ${i}`
-    const tabId = uuid.v1()
+    const tabId = uuid()
 
     const contentProps = {
       isAnalysis: type !== 'data',
