d30c02de0c5d048305d6625efdae4fc1a3afd1a3
Guillermo <gyanezfeliu@gmail.com>
2020-07-28 10:13:48 -0400

    merge changes

--- src/Components/Upload/ExtraInfo.js
+++ src/Components/Upload/ExtraInfo.js
@@ -6,20 +6,6 @@ import PropTypes from 'prop-types'
 
 /** Form for assay metadata sumbission */
 const ExtraInfo = ({ onSubmit, extraInfoFields, loading = false, assayId }) => {
-<<<<<<< HEAD
-  const steps = Object.entries(extraInfoFields).map(([type, fields]) => ({
-    title: type,
-    fields: fields.map((field, i) => ({
-      name: `${type}-${i}`,
-      label: field,
-      showLabel: true,
-      rules: [{ required: true }],
-      RenderField: SelectOrCreate,
-      extraCreationValues: { assays: [assayId] },
-      ...getFieldParams(type),
-    })),
-  }))
-=======
   const steps = Object.entries(extraInfoFields)
     .filter(([, fields]) => fields.length)
     .map(([type, fields]) => ({
@@ -34,7 +20,6 @@ const ExtraInfo = ({ onSubmit, extraInfoFields, loading = false, assayId }) => {
         ...getFieldParams(type),
       })),
     }))
->>>>>>> dev
 
   return (
     <SteppedFormFactory
